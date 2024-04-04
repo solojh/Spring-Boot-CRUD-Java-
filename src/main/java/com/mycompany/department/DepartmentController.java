b90bd9c15a07daf501c5325b77876c090fc2a183
solojh <97234810+solojh@users.noreply.github.com>
2024-04-04 21:41:20 +0800

    update test

--- src/main/java/com/mycompany/department/DepartmentController.java
+++ src/main/java/com/mycompany/department/DepartmentController.java
@@ -57,4 +57,8 @@ public class DepartmentController {
         }
         return "redirect:/departments";
     }
+    @GetMapping("/index")
+    public String goToIndexPage() {
+        return "index"; // Thymeleaf template name
+    }
 }
